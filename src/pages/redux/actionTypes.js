e4172d94e45699822e6f8290788d21557db8ca4e
Wizard Young <rcvbuf@gmail.com>
2021-03-08 00:45:20 +0800

    add product folder to use github pages

---
+++ src/pages/redux/actionTypes.js
@@ -0,0 +1,4 @@
+export const WALLET_OPEN = "WALLET_OPEN";
+export const WALLET_BALANCE = "WALLET_BALANCE";
+export const WALLET_SEND = "WALLET_SEND";
+export const WALLET_RECEIVE = "WALLET_RECEIVE";
---
+++ src/pages/redux/actions.js
@@ -0,0 +1,25 @@
+import { WALLET_OPEN } from "./actionTypes";
+
+// open wallet not really open the wallet.
+// it save the wallet name and wallet password. (with verifycation of it)
+// passwowrd can be stolen. but the key is safe.
+// the private key is stored & encrypted by the password. 
+// when use, private key is decrypted and used and then disposed.
+// by this way we keep the private key safe
+
+// about multiple wallet
+// wallets: {"default", "big one", "small one"}
+// defult: password protected binary data
+// after open wallet, save {name: 'wallet name', pass: 'password'} 
+
+// save to persistant storage provided by localforage, named 'rxstor'
+// [{name: 'wallet name', data: '[base64(password encrypted binary data)]'}, { ...} ...]
+
+export const openWallet = (name, password) => ({
+  type: WALLET_OPEN,
+  payload: {
+    name: name,
+    password: password
+  }
+});
+
