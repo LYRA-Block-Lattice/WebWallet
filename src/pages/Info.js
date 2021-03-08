c1e1d9e8b7cc3d3e9926202cdff1c5e8197ec131
Wizard Young <rcvbuf@gmail.com>
2021-03-08 23:43:47 +0800

    let user backup private key.

--- src/pages/Info.js
+++ src/pages/Info.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import { Button } from 'antd';
+import "antd/dist/antd.css";
+
 import LyraCrypto from '../lyra/crypto';
 import persist from '../lyra/persist';
 
@@ -7,8 +10,10 @@ export default class Info extends Component {
         super(props);
         this.state = { 
             network: '',
-            pub: ''
+            pub: '',
+            pvt: '',
           };
+          this.shwopvt = this.shwopvt.bind(this);
     }
 
     async componentDidMount() {
@@ -24,18 +29,38 @@ export default class Info extends Component {
         this.setState({pub: pub});
     }
 
+    async shwopvt() {
+        const tokenString = sessionStorage.getItem('token');
+        const token = JSON.parse(tokenString);
+
+        var pdata = await persist.getData();
+        var wallets = pdata.wallets;
+        var decData = LyraCrypto.decrypt(wallets[0].data, token);
+        var pvk = LyraCrypto.lyraDec(decData);
+        var pvtkey = LyraCrypto.lyraEncPvt(pvk);
+        this.setState({pvt: pvtkey});
+    }
+
     render() {
         // if (!token) {
         //     return <OpenWallet setToken={setToken} />
         // }
     
-        return (
+        if(this.state.pvt !== '')
+            return (<div>
+                <p>Your private key is:</p>
+                <pre>{this.state.pvt}</pre>
+                <p>Please save it properly.</p>
+            </div>);
+
+        return (            
             <div>
                 <h2>Information about your wallet</h2>
                 <p>Your wallet address is:</p>
                 <pre>{this.state.pub}</pre>
                 <p>Current connected blockchain:</p>
                 <pre>{this.state.network}</pre>
+                <Button type="link" onClick={this.shwopvt}>Show my private key</Button>
             </div>
             
         );
