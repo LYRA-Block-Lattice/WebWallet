abd2b50f4b98d472d412fec03183df89ea8afae9
Wizard Young <rcvbuf@gmail.com>
2021-04-03 14:34:10 +0800

    force refresh on startup

--- src/App.js
+++ src/App.js
@@ -7,27 +7,45 @@ import { Button } from "@material-ui/core";
 
 import Main from './pages/Main';
 
+const isIOS = !!global.navigator.userAgent && /iPad|iPhone|iPod/.test(global.navigator.userAgent)
+  
+const getAppVersion = () => {
+  const url = `/version.json?c=${Date.now()}`
+  const headers = {
+    'Cache-Control': 'no-cache, no-store, must-revalidate'
+  }
+
+  return fetch(url, { headers }).then(response => response.json())
+}
+
+const handleScreenFocus = () => {
+  if (true) {
+    const currentVersion = global.localStorage.getItem('appVersion')
+
+    getAppVersion()
+      .then((data) => {
+        if (data.version !== currentVersion) {
+          console.log("Found new version.");
+          global.localStorage.setItem('appVersion', data.version)
+
+          window.location.reload()
+        }
+      })
+  }
+}
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {
-      newVersionAvailable: false,
-      waitingWorker: {},
-    };
+		this.state = {
+			promptRefresh: false,
+		};
   }
-
-  onServiceWorkerUpdate = (registration) => {
-    this.setState({
-      waitingWorker: registration && registration.waiting,
-      newVersionAvailable: true,
-    });
-  };
 
   updateServiceWorker = () => {
     const { waitingWorker } = this.state;
     waitingWorker && waitingWorker.postMessage({ type: "SKIP_WAITING" });
-    this.setState({ newVersionAvailable: false });
+    this.setState({ promptRefresh: false });
     window.location.reload();
   };
 
@@ -45,21 +63,30 @@ class App extends Component {
     );
   };
 
-
-  componentDidMount = () => {
+	componentDidMount() {
     const { enqueueSnackbar } = this.props;
-    const { newVersionAvailable } = this.state;
-    if (process.env.NODE_ENV === 'production') {
-      serviceWorker.register({ onUpdate: this.onServiceWorkerUpdate });
-    }
 
-    if (newVersionAvailable) //show snackbar with refresh button
-      enqueueSnackbar("A new version was released", {
-        persist: true,
-        variant: "success",
-        action: this.refreshAction(),
-      });
-  };
+    handleScreenFocus();
+
+    if (this.state.promptRefresh) //show snackbar with refresh button
+    enqueueSnackbar("A new version was released", {
+      persist: true,
+      variant: "success",
+      action: this.refreshAction(),
+    });
+	}
+
+	handleRefreshForUpdate = () => {
+		if ('serviceWorker' in navigator) {
+			navigator.serviceWorker
+				.getRegistration()
+				.then((reg) => {
+					reg.waiting.postMessage({ type: 'SKIP_WAITING' });
+					window.location.reload();
+				})
+				.catch((err) => console.log('Could not get registration: ', err));
+		}
+	};
 
   render() {
     return (
--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -3,7 +3,8 @@ export default function Welcome(props) {
       <div>
         <h1>Hello, Welcome to the Lyrian world!</h1>
         <br/><br/><br/><br/><br/><br/><br/><br/>
-        <p><i>Pigs never die. They always get killed.</i></p>
+        <p>.......</p>
+        <p><i>Lyra Inc., All Rights Reserved.</i></p>
       </div>      
     );
   }
