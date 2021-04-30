bb6478de978724726d2333dcac91511ea0444d03
wizd <rcvbuf@gmail.com>
2021-05-01 02:16:02 +0800

    disable offline first

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { SnackbarProvider } from 'notistack';
+import React from "react";
+import ReactDOM from "react-dom";
+import { SnackbarProvider } from "notistack";
 
-import './index.css';
-import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import "./index.css";
+import App from "./App";
+import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
+import reportWebVitals from "./reportWebVitals";
 
-import { Provider } from 'react-redux';
-import { ConnectedRouter } from 'connected-react-router'
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "connected-react-router";
 
-import configureStore, { history } from './pages/redux/store';
+import configureStore, { history } from "./pages/redux/store";
 
 const store = configureStore();
 
@@ -20,17 +20,17 @@ ReactDOM.render(
       <ConnectedRouter history={history}>
         <SnackbarProvider>
           <App />
-        </SnackbarProvider>        
+        </SnackbarProvider>
       </ConnectedRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorkerRegistration.register();
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
